716be038b0c26b3fa7e4ecef630a3d6ad70bd789
Rafael <rafaelvleite@icloud.com>
2021-05-25 10:43:02 -0300

    annyang improvements

--- content.js
+++ content.js
@@ -8,18 +8,10 @@ function enableFreedomMode() {
 
     // enableMouseCoordinatesDebug();
     
-    
-    // Event listeners for daily games and promotion
-    const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
-    const script = document.createElement('script');
-    script.setAttribute("type", "module");
-    script.setAttribute("src", chrome.extension.getURL('builtFunctions/eventListeners.js'));
-    head.insertBefore(script, head.lastChild);
-    
     // Text to Speech
     window['speech'] = new SpeechSynthesisUtterance();
     speech.lang = "pt-BR";
-    speech.rate = 1.1;
+    speech.rate = 0.8;
     speech.pitch = 1;
     speech.volume = 1;
     
@@ -31,18 +23,13 @@ function enableFreedomMode() {
     
     // speech.text = welcomeMessage;
     // window.speechSynthesis.speak(speech);
-
-    // Start listening.
-    // recognition.start();
-        
+    
     // start observing opponent's moves
     startObservingMoves();
-    
      
     // start recognizing speech
     annyang.setLanguage('pt-BR');
     annyang.start();
-    
     
 }
 
